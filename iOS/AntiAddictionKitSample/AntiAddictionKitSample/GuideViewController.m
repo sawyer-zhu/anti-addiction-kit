b12c6eff943f83a0728bbd1ce1dc59db18d12781
Jinya <ttwotu@qq.com>
2020-04-07 15:44:12 +0800

    更新 iOS 文档

--- iOS/AntiAddictionKitSample/AntiAddictionKitSample/GuideViewController.m
+++ iOS/AntiAddictionKitSample/AntiAddictionKitSample/GuideViewController.m
@@ -13,7 +13,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    self.textView.text = @"使用步骤：\n\nApp启动\n\n1.配置sdk功能（可选）\n\n⇩\n\n2.配置用户时长（可选）\n\n⇩\n\n3.初始化sdk（必选！！！）\n\n⇩\n\n4.设置用户setUser\n（id不能为空，空=退出登录）\n\n\n\n\n\n生成证件号功能，仅 DEBUG 打包测试可用";
+    self.textView.text = @"使用步骤：\n\nApp启动\n\n1.配置sdk功能（可选）\n\n⇩\n\n2.配置用户时长（可选）\n\n⇩\n\n3.初始化sdk（必选！！！）\n\n⇩\n\n4.设置用户setUser\n（id不能为空，空=退出登录）\n\n\n\n\n\n";
     
     [self.closeButton addTarget:self action:@selector(dismiss) forControlEvents:UIControlEventTouchUpInside];
 }
